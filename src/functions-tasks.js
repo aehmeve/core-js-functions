04919ff061776d57854ab29f5547c0216aaf5928
aehmeve <ehmorizehere@gmail.com>
2024-04-29 03:03:29 +0900

    feat: solve some tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,11 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (!func) {
+    return '';
+  }
+  return func.toString();
 }
 
 /**
@@ -50,8 +53,12 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const countArr = [];
+  funcs.forEach((func) => {
+    countArr.push(func.length);
+  });
+  return countArr;
 }
 
 /**
@@ -70,8 +77,13 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const expon = exponent;
+  function mathPower(number) {
+    const result = number ** expon;
+    return result;
+  }
+  return mathPower;
 }
 
 /**
