fd62fb061efa18f9d7a43d037f536b113b97c22f
aehmeve <ehmorizehere@gmail.com>
2024-05-02 03:32:20 +0900

    feat: solve some tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -99,8 +99,16 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (!args.length) return null;
+  function calcResult(x) {
+    let result = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      result += args[i] * x ** (args.length - i - 1);
+    }
+    return result;
+  }
+  return calcResult;
 }
 
 /**
@@ -117,8 +125,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  return () => {
+    if (!cache) {
+      cache = func();
+      return cache;
+    }
+    return cache;
+  };
 }
 
 /**
