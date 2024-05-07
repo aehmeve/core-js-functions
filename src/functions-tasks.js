5d95654ac7b8102ab72e0b7a7252474c6a1fa8b3
aehmeve <ehmorizehere@gmail.com>
2024-05-07 23:41:54 +0900

    feat: solve partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -211,8 +211,12 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => {
+    const allArgs = [...args1, ...args];
+    const result = fn(...allArgs);
+    return result;
+  };
 }
 
 /**
